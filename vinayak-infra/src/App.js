265da180244d8e6cad0d8b49b9d87d39428f3700
Saahas12 <76211083+Saahas12@users.noreply.github.com>
2023-06-14 21:54:58 +0530

    Edited App.js

--- vinayak-infra/src/App.js
+++ vinayak-infra/src/App.js
@@ -6,7 +6,11 @@ import Projects from "./routes/Projects";
 import ProjectComponent from "./components/ProjectComponent";
 import ProjectOngoing from "./components/ProjectOngoing";
 import { Rent } from "./routes/Rent";
-
+import Laxmiraaj from "./components/Laxmiraaj";
+import Laxmishru from "./components/Laxmishru";
+import Laxmisans from "./components/Laxmisans";
+import Shantiheight from './components/Shantiheight'
+import Contact from "./routes/contact"
 
 function App() {
   return (
@@ -17,6 +21,11 @@ function App() {
       <Route path="/projectscom" element={<ProjectComponent/>}/>
       <Route path="/projectsong" element={<ProjectOngoing/>}/>
       <Route path="/rent" element={<Rent/>}/>
+      <Route path="/laxmi_raj" element={<Laxmiraaj/>}/>
+      <Route path="/laxmi_shru" element={<Laxmishru/>}/>
+      <Route path="/laxmi_sans" element={<Laxmisans/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/shanti_height" element={<Shantiheight/>}/>
     </Routes>
     </>
   );
